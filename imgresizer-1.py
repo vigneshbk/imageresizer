e789be2f827fad397f232e94b553e04f719b934b
VigneshKBandsale <evigkum@localhost.localdomain>
2015-09-30 18:11:07 +0100

    version 1

--- imgresizer-1.py
+++ imgresizer-1.py
@@ -2,6 +2,7 @@ __author__ = 'evigkum'
 
 import yaml
 import os
+import Image
 
 inputDirs=[]
 resultDir=''
@@ -21,19 +22,33 @@ def loadInput():
           else:
                print 'BoseDK'
 
-
+def listFolders(parentFolderName):
+      folderList = []
+      for (dirpath, subdirs, filenames) in os.walk(parentFolderName):
+          for dirO in subdirs:
+               folderList.append(dirpath+'/'+dirO)
+      return sorted(folderList)
 
 def listFiles(folderName):
-     #for (dirpath, dirnames, filenames) in os.walk(folderName):
-     alist_filter = ['jpg','bmp','png','gif']
-     #for imageFile in os.listdir(folderName):
-     #     if imageFile[-3:] in alist_filter:
-     #          #print imageFile
+      alist_filter = ['jpg','bmp','png','gif','jpeg','img']
+      allimgs = [(folderName, imageFile) for imageFile in os.listdir(folderName) if os.path.splitext(imageFile)[1][1:] in alist_filter]
+      return allimgs
 
-     allimgs = [imageFile for imageFile in os.listdir(folderName) if imageFile[-3:] in alist_filter]
-     print allimgs
 
+def resizeFile(imageList):
+      size = 128, 128
+      for infile in imageList:
+          outfile = '/home/evigkum/testbed-op'+'/'+ os.path.splitext(infile[1])[0] + ".jpeg"
+          if infile != outfile:
+               try:
+                    im = Image.open(infile[0] + '/' +infile[1])
+                    im.thumbnail(size)
+                    im.save(outfile, "JPEG")
+               except IOError as e:
+                    print "cannot create thumbnail for", (infile[0] + '/' +infile[1])+e.strerror
 
 
 loadInput()
-listFiles(inputDirs[0])
+
+print(listFiles(listFolders(inputDirs[0])[1]))
+resizeFile(listFiles(listFolders(inputDirs[0])[1]))
